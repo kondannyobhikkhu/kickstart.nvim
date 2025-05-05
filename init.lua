6a1bf1888605841a95f82ff8dca9b49e2c8741e4
kondannyo bhikkhu <kondannyo@protonmail.com>
2025-05-05 08:27:12 -0400

    added render-markdown

--- init.lua
+++ init.lua
@@ -1007,13 +1007,13 @@ require('lazy').setup({
   require 'kickstart.plugins.neo-tree',
   -- require 'kickstart.plugins.gitsigns', -- adds gitsigns recommend keymaps
 
-  require('custom.split_view').setup(),
-  require('custom.sutta_navigator').setup(),
+  --  require('custom.split_view').setup(),
+  --  require('custom.sutta_navigator').setup(),
   -- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
   --    This is the easiest way to modularize your config.
   --
   --  Uncomment the following line and add your plugins to `lua/custom/plugins/*.lua` to get going.
-  --  { import = 'custom.plugins' },
+  { import = 'custom.plugins' },
   --
   -- For additional information with loading, sourcing and examples see `:help lazy.nvim-🔌-plugin-spec`
   -- Or use telescope!
